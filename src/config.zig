834421b65b10dafe36ae5add10d7bbdb804b2081
fawn <fawn@rinici.de>
2023-12-21 07:03:54 +0200

    ✨ feature: use a `config.json`

---
+++ src/config.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+
+const string = []const u8;
+
+const Config = struct { address: string, port: u16 };
+
+pub fn read(allocator: std.mem.Allocator) !Config {
+    const file = try std.fs.cwd().readFileAlloc(allocator, "config.json", 4096);
+    var parsed = try std.json.parseFromSlice(Config, allocator, file, .{});
+    defer parsed.deinit();
+
+    return parsed.value;
+}
--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,9 @@
 const std = @import("std");
 const server = @import("server.zig");
 const mimes = @import("mimes.zig");
-const http = std.http;
+const config = @import("config.zig");
 
 const log = std.log.scoped(.main);
-const Args = [][:0]u8;
-
-const AprilError = error{
-    ArgsMismatch,
-};
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -19,35 +14,14 @@ pub fn main() !void {
     try mimes.init(allocator);
     defer mimes.deinit(allocator);
 
-    // TODO: use a config.json for address, port and eventually token
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
-
-    if (args.len != 3) {
-        log.err("arguments mismatched", .{});
-        printUsage(args);
-        return AprilError.ArgsMismatch;
-    }
-
-    const address = args[1];
-    const port = std.fmt.parseUnsigned(u16, args[2], 10) catch |err| {
-        log.err("parsing port: {}", .{err});
-        printUsage(args);
-        return err;
-    };
-
-    server.run(.{ .address = address, .port = port, .allocator = allocator }) catch |err| {
+    const cfg = try config.read(allocator);
+    server.run(.{ .address = cfg.address, .port = cfg.port, .allocator = allocator }) catch |err| {
         log.err("server error: {}\n", .{err});
         if (@errorReturnTrace()) |trace| {
             std.debug.dumpStackTrace(trace.*);
         }
         std.os.exit(1);
     };
-}
-
-fn printUsage(args: Args) void {
-    const stdout = std.io.getStdOut().writer();
-    stdout.print("Usage: {s} <host> <port>\n", .{args[0]}) catch {};
 }
 
 test {
