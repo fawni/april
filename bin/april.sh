93c1798a80e8ca2461aea65df5dcb0365ba050ab
fawn <fawn@rinici.de>
2024-04-13 06:08:06 +0200

    ♻️ refactor(april.sh): remove unneccessary check

--- bin/april.sh
+++ bin/april.sh
@@ -5,7 +5,6 @@ CONFIG=$HOME/.config/april/config.json
 
 test -f "$CONFIG" && TOKEN=$(cat "$CONFIG" | jq -er .token)
 FILE=$(gum file)
-test "$FILE" || exit 1
 
 test "$TOKEN" || TOKEN=$(gum input --placeholder "april token" --password)
 RESPONSE=$(curl -sfF "file=@$FILE" "$HOST" -H "Authorization: $TOKEN")
